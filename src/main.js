49f13c95a93a0110a94fd50f12e4cdfc8d41066f
Niharika31 <niharika31saxena@gmail.com>
2024-05-23 14:34:06 +0530

    init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import store from './store';
 
-loadFonts()
-
-const app = createApp(App);
-
-app.use(vuetify);
-app.use(store); // Use the store
-app.mount('#app');
+createApp(App).mount('#app')
